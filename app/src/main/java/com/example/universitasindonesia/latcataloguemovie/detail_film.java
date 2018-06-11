9d4455c0389d22d5408e6e28b5d3b0920800e0b4
Jasminenabila <jasminenabila19@gmail.com>
2018-06-11 23:03:25 +0700

    first commit

---
+++ app/src/main/java/com/example/universitasindonesia/latcataloguemovie/detail_film.java
@@ -0,0 +1,13 @@
+package com.example.universitasindonesia.latcataloguemovie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class detail_film extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_film);
+    }
+}
